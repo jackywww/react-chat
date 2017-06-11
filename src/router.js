388049ff5ea8f9f8ac68d63aea970836365731f1
weiliu <503354703@qq.com>
2017-06-11 23:20:08 +0800

    antd

--- src/router.js
+++ src/router.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Router, Route } from 'dva/router';
 import IndexPage from './routes/IndexPage';
-import ClientPage from './routes/ClientPage';
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Route path="/" component={IndexPage} />
-      <Route path="/client" component={ClientPage} />
     </Router>
   );
 }
