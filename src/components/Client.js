c8dafd861c610589d6858264062fed135727d1ef
weiliu <503354703@qq.com>
2017-06-11 20:40:26 +0800

    ddd

---
+++ src/components/Client.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Client = () => {
+  return (
+    <div>
+      Example
+    </div>
+  );
+};
+
+Client.propTypes = {
+};
+
+export default Client;
--- src/routes/ClientPage.js
+++ src/routes/ClientPage.js
@@ -1,21 +1,105 @@
 import React from 'react';
 import { connect } from 'dva';
-import styles from './IndexPage.css';
+import { Flex, TabBar, Icon } from 'antd-mobile';
+import styles from './ClientPage.css';
 
-function IndexPage() {
-  return (
-    <div className={styles.normal}>
-      <h1 className={styles.title}>Yay! Welcome to dva!</h1>
-      <div className={styles.welcome} />
-      <ul className={styles.list}>
-        <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
-        <li><a href="https://github.com/dvajs/dva-docs/blob/master/v1/en-us/getting-started.md">Getting Started</a></li>
-      </ul>
-    </div>
+const ClientPage = () => {
+
+	return (
+		  <TabBar
+	        unselectedTintColor="#949494"
+	        tintColor="#33A3F4"
+	        barTintColor="white"
+	        hidden={this.state.hidden}
+	      >
+	        <TabBar.Item
+	          title="生活"
+	          key="生活"
+	          icon={<div style={{
+	            width: '0.44rem',
+	            height: '0.44rem',
+	            background: 'url(https://zos.alipayobjects.com/rmsportal/sifuoDUQdAFKAVcFGROC.svg) center center /  0.42rem 0.42rem no-repeat' }}
+	          />
+	          }
+	          selectedIcon={<div style={{
+	            width: '0.44rem',
+	            height: '0.44rem',
+	            background: 'url(https://zos.alipayobjects.com/rmsportal/iSrlOTqrKddqbOmlvUfq.svg) center center /  0.42rem 0.42rem no-repeat' }}
+	          />
+	          }
+	          selected={this.state.selectedTab === 'blueTab'}
+	          badge={1}
+	          onPress={() => {
+	            this.setState({
+	              selectedTab: 'blueTab',
+	            });
+	          }}
+	          data-seed="logId"
+	        >
+	          {this.renderContent('生活')}
+	        </TabBar.Item>
+	        <TabBar.Item
+	          icon={<Icon type="koubei-o" size="md" />}
+	          selectedIcon={<Icon type="koubei" size="md" />}
+	          title="口碑"
+	          key="口碑"
+	          badge={'new'}
+	          selected={this.state.selectedTab === 'redTab'}
+	          onPress={() => {
+	            this.setState({
+	              selectedTab: 'redTab',
+	            });
+	          }}
+	          data-seed="logId1"
+	        >
+	          {this.renderContent('口碑')}
+	        </TabBar.Item>
+	        <TabBar.Item
+	          icon={
+	            <div style={{
+	              width: '0.44rem',
+	              height: '0.44rem',
+	              background: 'url(https://zos.alipayobjects.com/rmsportal/psUFoAMjkCcjqtUCNPxB.svg) center center /  0.42rem 0.42rem no-repeat' }}
+	            />
+	          }
+	          selectedIcon={
+	            <div style={{
+	              width: '0.44rem',
+	              height: '0.44rem',
+	              background: 'url(https://zos.alipayobjects.com/rmsportal/IIRLrXXrFAhXVdhMWgUI.svg) center center /  0.42rem 0.42rem no-repeat' }}
+	            />
+	          }
+	          title="朋友"
+	          key="朋友"
+	          dot
+	          selected={this.state.selectedTab === 'greenTab'}
+	          onPress={() => {
+	            this.setState({
+	              selectedTab: 'greenTab',
+	            });
+	          }}
+	        >
+	          {this.renderContent('朋友')}
+	        </TabBar.Item>
+	        <TabBar.Item
+	          icon={{ uri: 'https://zos.alipayobjects.com/rmsportal/asJMfBrNqpMMlVpeInPQ.svg' }}
+	          selectedIcon={{ uri: 'https://zos.alipayobjects.com/rmsportal/gjpzzcrPMkhfEqgbYvmN.svg' }}
+	          title="我的"
+	          key="我的"
+	          selected={this.state.selectedTab === 'yellowTab'}
+	          onPress={() => {
+	            this.setState({
+	              selectedTab: 'yellowTab',
+	            });
+	          }}
+	        >
+	          {this.renderContent('我的')}
+	        </TabBar.Item>
+	      </TabBar>
   );
 }
 
-IndexPage.propTypes = {
+ClientPage.propTypes = {
 };
 
-export default connect()(IndexPage);
+export default connect()(ClientPage);
