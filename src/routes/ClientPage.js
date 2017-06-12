b3c140ee9189891d99bda9e56464f47be92051e2
weiliu <503354703@qq.com>
2017-06-12 23:24:19 +0800

    aaa

--- src/routes/ClientPage.js
+++ src/routes/ClientPage.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import { connect } from 'dva';
 import styles from './IndexPage.css';
-import { Layout, Row, Col, Input } from 'antd';
+import { Layout, Row, Col, Input, Icon } from 'antd';
 const { Header, Footer, Content } = Layout;
-const Search = Input.Search;
-console.log(Input)
+
 function ClientPage() {
   return (
-    <div style={{ width: '680px', margin: '0 auto' }}>
+    <div style={{ maxWidth: '680px', width: '100%', margin: '0 auto' }}>
 		<Layout>
-			<Content style={{ position: 'fixed', width: '680px', top: '0', bottom: '50px', background: '#eaeaea' }}>Content</Content>
-			<Footer style={{ position: 'fixed', width: '680px', height: '50px', bottom: '0', padding: '0' }}>
-			<Search
-				placeholder="input search text"
-				style={{ width: '100%' }}
-				onSearch={value => console.log(value)}
-			/>
+			<Content style={{ position: 'fixed', width: '100%', maxWidth: '680px', top: '0', bottom: '50px', background: '#eaeaea' }}>Content</Content>
+			<Footer style={{ position: 'fixed', width: '100%', maxWidth: '680px', height: '50px', bottom: '0', padding: '0' }}>
+		      <Input style={{ height: '34px', marginTop: '7px', float: 'left', width: '80%' }} />
 			</Footer>
 		</Layout>
     </div>
