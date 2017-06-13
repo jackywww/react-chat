88a4a3fd960c5d788f270639d20f2ddff6b3393f
刘伟 <liuwei@17shihui.com>
2017-06-13 09:55:59 +0800

    bbb

--- src/routes/ClientPage.js
+++ src/routes/ClientPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'dva';
 import styles from './IndexPage.css';
-import { Layout, Row, Col, Input, Icon } from 'antd';
+import { Layout, Button, Input, Icon } from 'antd';
 const { Header, Footer, Content } = Layout;
 
 function ClientPage() {
@@ -10,7 +10,9 @@ function ClientPage() {
 		<Layout>
 			<Content style={{ position: 'fixed', width: '100%', maxWidth: '680px', top: '0', bottom: '50px', background: '#eaeaea' }}>Content</Content>
 			<Footer style={{ position: 'fixed', width: '100%', maxWidth: '680px', height: '50px', bottom: '0', padding: '0' }}>
-		      <Input style={{ height: '34px', marginTop: '7px', float: 'left', width: '80%' }} />
+		      		<Input style={{ height: '34px', marginTop: '7px', float: 'left', width: '80%' }} />
+		      		<Button style={{ float: 'left', height: '34px', marginTop: '7px', marginLeft:'1%' }} type="primary">发送</Button>
+		      		<Icon style={{ float: 'left', fontSize: '30px', marginTop: '8px', marginLeft:'1%' }} type="plus-circle-o" />
 			</Footer>
 		</Layout>
     </div>
