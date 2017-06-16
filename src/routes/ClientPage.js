f238007091d5f090ecb4ce612ea6479adae65aba
刘伟 <liuwei@17shihui.com>
2017-06-16 14:24:41 +0800

    bak

--- src/routes/ClientPage.js
+++ src/routes/ClientPage.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import { connect } from 'dva';
-import styles from './IndexPage.css';
-import { Layout, Button, Input, Icon, Row, Col } from 'antd';
+import { Layout, Button, Input, Icon, Row, Col, Popover } from 'antd';
 const { Header, Footer, Content } = Layout;
-
+import styles from './IndexPage.less';
+const content = (
+		  <div>
+		    <p>Contentaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+		  </div>
+		);
 function ClientPage() {
   return (
     <div style={{ maxWidth: '680px', width: '100%', margin: '0 auto' }}>
 		<Layout>
-			<Content style={{ position: 'fixed', width: '100%', maxWidth: '680px', top: '0', bottom: '50px', background: '#eaeaea' }}>Content</Content>
+			<Content style={{ position: 'fixed', width: '100%', maxWidth: '680px', top: '0', bottom: '50px', background: '#eaeaea' }}>	
+				<div className="chatpop left">aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</div>
+			</Content>
 			<Footer style={{ position: 'fixed', width: '100%', maxWidth: '680px', height: '50px', bottom: '0', padding: '0' }}>
 				<Row gutter={8} style={{ textAlign: 'center', marginTop: '10px' }}>
 				  <Col span={17}><Input style={{ width: '95%' }} /></Col>
-				  <Col span={4}><Button style={{ width: '95%', height: '28px' }} type="primary">发送</Button></Col>
+				  <Col span={4}><Button style={{ width: '95%', height: '28px', padding: '0' }} type="primary">发送</Button></Col>
 				  <Col span={3}><Icon style={{ fontSize: '26px' }} type="plus-circle-o" /></Col>
 				</Row>  		
 			</Footer>
