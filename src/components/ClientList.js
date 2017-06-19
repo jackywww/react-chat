cfa801f09f06288d3116e06c208f56a0a9043101
刘伟 <liuwei@17shihui.com>
2017-06-19 13:15:01 +0800

    bak

---
+++ src/components/ClientList.js
@@ -0,0 +1,15 @@
+import React, {propTypes} from 'react';
+import { Layout, Button, Input, Icon, Row, Col, Avatar } from 'antd';
+const { Header, Footer, Content } = Layout;
+
+const ClientList = () => {	
+	return (
+	    <div style={{ maxWidth: '680px', width: '100%', margin: '0 auto' }}>
+
+	    </div>
+	  );
+};
+
+ClientList.propTypes = {
+};
+export default ClientList;
--- src/routes/ClientPage.js
+++ src/routes/ClientPage.js
@@ -1,28 +1,18 @@
 import React from 'react';
 import { connect } from 'dva';
-import ChatList from '../components/ChatList';
 
-const ChatPage = ({ dispatch, chatlist }) => {
-  function handleChange(send) {
-	//console.log(send.target.value)
-    dispatch({
-      type: 'chatlist/change',
-      payload: send,
-    });
-  }
-  function handleSend() {
-	dispatch({
-      type: 'chatlist/send',
-    });
-  }
+import ClientList from '../components/ClientList';
+
+const ClientPage = ({}) => {
+
   return (
     <div>
-      <ChatList onChange={handleChange} onSend={handleSend} chatlist={chatlist} />
+
     </div>
   );
 };
 
 // export default Products;
-export default connect(({ chatlist }) => ({
-  chatlist,
-}))(ChatPage);
+export default connect(({}) => ({
+
+}))(ClientPage);
